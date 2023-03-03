cbab2fcedbe02243209a51d40ace1277c802698a
Bruce <recall4056@gmail.com>
2023-03-04 00:53:06 +0800

    fix: remove fall back view

--- site/src/views/Config/index.vue
+++ site/src/views/Config/index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
-<template><main>test</main></template>
+<template>
+  <main>
+    <div>Test Page</div>
+  </main>
+</template>
 
 <style scoped lang="scss"></style>

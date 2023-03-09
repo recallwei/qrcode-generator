3bbcc22a02c2749fa500951a3f107eb0e294a813
Bruce <recall4056@gmail.com>
2023-03-09 10:04:21 +0800

    feat: add naive custom selector option type

--- site/src/types/naive.ts
+++ site/src/types/naive.ts
@@ -2,3 +2,12 @@
  * @description Corresponding to naiveUI Type `FormValidationStatus`
  */
 export type ValidationStatus = "success" | "error" | "warning"
+
+/**
+ * @description NSelect option type
+ */
+export type SelectorOption<T> = {
+  label: string
+  value: string | number
+  data: T
+}

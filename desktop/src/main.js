79949f57cf032841ab79317c9bb4ab7e92fd3b5f
Bruce <recall4056@gmail.com>
2023-03-10 14:43:55 +0800

    feat: add notification and icon

--- desktop/src/main.js
+++ desktop/src/main.js
@@ -1,33 +1,56 @@
-const { app, BrowserView, BrowserWindow } = require("electron")
+const { app, BrowserView, BrowserWindow, Notification } = require("electron")
+const path = require("path")
+
+function showNotification() {
+  new Notification({
+    title: "二维码生成器",
+    body: "当前处于内测版本，出现 Bug 请反馈给管理员。",
+    silent: false
+  }).show()
+}
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 1920,
-    height: 1080,
-    icon: "/src/images/qrcode-favicon.png"
+    x: 0,
+    y: 0,
+    width: 1600,
+    height: 900,
+    minWidth: 800,
+    minHeight: 600,
+    icon: path.join(__dirname, "./images/favicon.ico")
+    // frame: false
   })
+
+  win.once("ready-to-show", () => {
+    win.show()
+  })
+  win.setAppDetails({ relaunchDisplayName: "二维码生成器" })
 
   const view = new BrowserView()
   win.setBrowserView(view)
-  view.setBounds({ x: 0, y: 0, width: 1920, height: 1080 })
+  view.setBounds({ x: 0, y: 0, width: 1600, height: 900 })
   view.setAutoResize({
     width: true,
     height: true,
-    horizontal: false,
-    vertical: false
+    horizontal: true,
+    vertical: true
   })
-  view.webContents.loadURL("https://qrcode.bruceworld.top")
+  view.webContents.loadURL("http://localhost:4058")
+  // view.webContents.loadURL("https://qrcode.bruceworld.top")
 }
 
-app.whenReady().then(() => {
-  createWindow()
+app
+  .whenReady()
+  .then(() => {
+    createWindow()
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
   })
-})
+  .then(showNotification)
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {

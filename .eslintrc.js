3ebce6efa3c9f25965c2a24366480b238bec5858
Bruce <recall4056@gmail.com>
2023-03-18 19:49:13 +0800

    chore: add eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,40 @@
-require("@rushstack/eslint-patch/modern-module-resolution")
+require('@rushstack/eslint-patch/modern-module-resolution')
+const { defineConfig } = require('eslint-define-config')
 
-module.exports = {
+module.exports = defineConfig({
   root: true,
   globals: {},
   extends: [
-    "plugin:vue/vue3-recommended",
-    "@vue/eslint-config-typescript",
-    "airbnb-base",
-    "@vue/eslint-config-prettier"
+    'plugin:jsonc/recommended-with-jsonc',
+    'plugin:markdown/recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript',
+    'airbnb-base',
+    '@vue/eslint-config-prettier'
   ],
+  plugins: ['html'],
   parserOptions: {
-    ecmaVersion: "latest"
+    ecmaVersion: 'latest'
   },
+  overrides: [
+    {
+      files: ['*.json', '*.json5', '*.jsonc'],
+      parser: 'jsonc-eslint-parser'
+    }
+  ],
   rules: {
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
-    "comma-dangle": "off",
-    "vue/multi-word-component-names": ["error", { ignores: ["index"] }],
-    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
-    "import/extensions": "off",
-    "import/prefer-default-export": "off",
-    "import/newline-after-import": "off",
-    // for import ./public
-    "import/no-absolute-path": "off",
-    "import/no-unresolved": "off",
-    // use @typescript-eslint/no-unused-vars to check.
-    "no-unused-vars": "off",
-    // use @typescript-eslint/no-shadow to check.
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": "error"
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'comma-dangle': 'off',
+    'vue/multi-word-component-names': ['error', { ignores: ['index'] }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'import/newline-after-import': 'off',
+    'import/no-absolute-path': 'off', // for import ./public
+    'import/no-unresolved': 'off',
+    'no-unused-vars': 'off', // use @typescript-eslint/no-unused-vars to check.
+    'no-shadow': 'off', // use @typescript-eslint/no-shadow to check.
+    '@typescript-eslint/no-shadow': 'error'
   }
-}
+})

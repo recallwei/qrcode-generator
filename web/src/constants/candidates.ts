fb0a4a82539206522815dc5c2ead64428016e696
Bruce <recall4056@gmail.com>
2023-03-28 15:53:39 +0800

    fix: ts error

--- web/src/constants/candidates.ts
+++ web/src/constants/candidates.ts
@@ -4,6 +4,5 @@ export const valueTypeCandidates: { label: string; value: ValueType; disabled?: 
   { label: '字符串', value: 'string' },
   { label: '数字', value: 'number' },
   { label: '布尔值', value: 'boolean', disabled: true },
-  { label: '数组', value: 'array', disabled: true },
-  { label: '对象', value: 'object', disabled: true }
+  { label: '数组', value: 'array', disabled: true }
 ]
